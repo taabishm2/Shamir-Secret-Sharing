82f355dbc747ef69a8bbd5bf9407594ac24865f4
Tabish <taabishm2@gmail.com>
2019-07-24 20:41:08 +0530

    Field Size
    Coefficients and secrets must be within the field mentioned

--- Shamir Secret Sharing.py
+++ Shamir Secret Sharing.py
@@ -1,78 +1,59 @@
 import random
 from math import ceil
+from decimal import *
 
-def lagrange(shares,t):
+global field_size
+field_size = 496025623
+
+def tncombinex(shares): #Combines shares using Lagranges interpolation
+    '''shares is an array of shares being combined, t is the threshold in the scheme'''
+    global field_size
+    
     sums = 0
     prod_arr = []
-    if len(shares) < t:
-        raise Exception("Shares provided less than threshold. Secret generation not possible")
     for j in range(len(shares)):
         xj,yj = shares[j][0],shares[j][1]
-        prod = 1
+        prod = Decimal(1)
         for i in range(len(shares)):
             xi = shares[i][0]
-            if i != j: prod *= xi/(xi-xj)  
+            if i != j: prod *= Decimal(Decimal(xi-x)/(xi-xj))
         prod *= yj
-        sums += prod
-    return ceil(sums)
+        
+        sums += Decimal(prod) % Decimal(field_size)
+
+    ret = Decimal(sums) % field_size
+    if ret < 0: ret += field_size
+    return ret
+            
             
 
 def polynom(x,coeff):
+    '''Evaluates a polynomial in x with coeff being the coefficient matrix with a given x'''
+    global field_size
     y = 0
     for i in range(len(coeff)):
-        y += (x**(len(coeff)-i-1)) * coeff[i]
-    return y
+        y += (x**(len(coeff)-i-1)) * coeff[i] 
+    return y % field_size
 
-def code(key):
-    res = []
-    for i in key:
-        res.append(str(ord(i.upper())))
-    return int(''.join(res))
 
 def coeff(t,secret):
+    '''randomly generate a coefficient array for a polynomial with degree t-1 whose constant = secret'''
+    global field_size
+
     coeff = []
     for i in range(t-1):
-        coeff.append(random.randrange(0,secret))
+        coeff.append(random.randrange(0,field_size-1))
     coeff.append(secret)
+    
     return coeff
 
-def shares(n,m,secret):
+def tnshares(n,m,secret):
+    '''Split secret using SSS into n shares with threshold m'''
+    global field_size
+
     cfs = coeff(m,secret)
     shares = []
     for i in range(1,n+1):
         shares.append([i,polynom(i,cfs)])
+
     return shares
-
-def main():
-    print("1.Generate Shares 2.Reconstruct Secret")
-    i = int(input())
-    if i == 1:
-        print("Participants: ",end=" ")
-        n = int(input())
-        print("Threshold:    ",end=" ")
-        m = int(input())
-        print("Secret:       ",end=" ")
-        secret = int(input())
-        share = shares(n,m,secret)
-        for s in share:
-            print(s[0],s[1])
-        return
-    elif i == 2:
-        print("Threshold: ",end=" ")
-        thres = int(input())
-        print("No. of attempting shares: ",end=" ")
-        s_no = int(input())
-        s_arr = []
-        for c in range(1,s_no+1):
-            print("Enter share number:",end=" ")
-            share_no = int(input())
-            print("Enter share value:",end="")
-            inp = int(input())
-            s_arr.append([share_no,inp])
-        print("Secret is :",lagrange(s_arr,thres))
-        return
-        
-while True:
-    main()
-
-    
